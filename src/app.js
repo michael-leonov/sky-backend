fbc3e304ead7fea147e39eed00548a134b31fc56
michael-leonov <michael.leonov96@mail.ru>
2023-01-12 10:41:58 +0300

    finished requests with GET-parameters

--- src/app.js
+++ src/app.js
@@ -2,27 +2,60 @@ import * as http from "http";
 import "dotenv/config";
 import getUsers from "./modules/users";
 
-const HOST_NAME = process.env.HOST_NAME || "http://127.0.0.1";
+const host = "http://127.0.0.1";
 const PORT = process.env.PORT || 5000;
 
 const server = http.createServer((req, res) => {
-  if (req.url === "/users") {
+  const url = new URL(req.url, host);
+
+  res.setHeader("Content-Type", "text/plain");
+
+  if (url.search.startsWith("?hello")) {
+    const name = url.searchParams.get("hello");
+
+    if (name) {
+      res.statusCode = 200;
+      res.statusMessage = "OK";
+      res.write(`Hello, ${name}`);
+      res.end();
+
+      return;
+    }
+
+    res.statusCode = 400;
+    res.statusMessage = "Bad request";
+    res.write("Enter a name");
+    res.end();
+
+    return;
+  }
+
+  if (url.search.startsWith("?users")) {
     res.statusCode = 200;
     res.statusMessage = "OK";
-    res.setHeader = "Content-Type: application/json";
+    res.setHeader("Content-Type", "application/json");
     res.write(getUsers());
     res.end();
 
     return;
   }
 
-  res.statusCode = 200;
-  res.statusMessage = "OK";
-  res.setHeader = "Content-Type: text/plain";
-  res.write("Hello World!");
+  if (url.search === "") {
+    res.statusCode = 200;
+    res.statusMessage = "OK";
+    res.write("Hello World");
+    res.end();
+
+    return;
+  }
+
+  res.statusCode = 500;
+  res.statusMessage = "Server error";
+  res.setHeader("Content-Type", "text/plain");
+  res.write("");
   res.end();
 });
 
 server.listen(PORT, () =>
-  console.log(`Server start on http://${HOST_NAME}:${PORT}/`)
+  console.log(`Server start on http://${host}:${PORT}/`)
 );
